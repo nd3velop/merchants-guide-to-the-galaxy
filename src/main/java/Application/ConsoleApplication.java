9c50bff43e7ed747ed68e55882556e3b4c54b75e
Nadine Dithmer <ndevelop@mail.de>
2021-10-08 08:34:09 +0200

    Change Main class to ConsoleApplication

--- src/main/java/Application/ConsoleApplication.java
+++ src/main/java/Application/ConsoleApplication.java
@@ -8,7 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class ConsoleApplication {
     public static void main(String[] args) throws IOException{
         IOutputProcessor consoleOutputProcessor = new ConsoleOutputProcessor();
         InputProcessor inputProcessor = new InputProcessor(consoleOutputProcessor);

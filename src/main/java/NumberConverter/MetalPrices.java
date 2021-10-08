d2c02675ab6e1454048607cc33e0f58f6bb77d6d
Nadine Dithmer <ndevelop@mail.de>
2021-10-08 14:45:35 +0200

    Add Doc Comments to MetalPrices

--- src/main/java/NumberConverter/MetalPrices.java
+++ src/main/java/NumberConverter/MetalPrices.java
@@ -7,18 +7,39 @@ public class MetalPrices {
     private HashMap<Double, String> creditsToMetal = new HashMap<>();
     private HashMap<String, Double> metalToCredits = new HashMap<>();
 
+    /**
+     * Adds an price/metal key value pair
+     * @param credits   price of the metal
+     * @param metal     name of the metal
+     */
     public void put(Double credits, String metal) {
         this.creditsToMetal.put(credits, metal.toLowerCase());
         this.metalToCredits.put(metal.toLowerCase(), credits);
     }
+
+    /**
+     * Returns the price of a metal
+     * @param metal name of the metal
+     * @return price of the metal
+     */
     public double getCredits(String metal) {
         return metalToCredits.get(metal.toLowerCase());
     }
 
+    /**
+     * Returns the metal that has the given price
+     * @param credits price of a metal
+     * @return name of the metal
+     */
     public String getMetal(Double credits) {
         return creditsToMetal.get(credits);
     }
 
+    /**
+     * Checks weather the metal already exists
+     * @param metal name of the metal
+     * @return true if the metal already exists
+     */
     public boolean hasMetal(String metal){
         return metalToCredits.containsKey(metal);
     }
